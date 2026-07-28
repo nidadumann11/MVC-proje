ab476e4c3f0530c54440b1f2461168d32bd8abea
nida duman <nidadumann11@gmail.com>
2026-07-29 00:51:44 +0300

    İlk commit - Proje tamamlandı

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Proje2.Models;
+
+namespace Proje2.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = Repository.Applications;
+            return View(model);
+        }
+        public IActionResult Apply()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Apply([FromForm] Candidate model)
+        {
+            if (Repository.Applications.Any(c => c.Email.Equals(model.Email)))
+            {
+                ModelState.AddModelError("", "There is already application for you.");
+            }
+            if (ModelState.IsValid)
+            {
+                Repository.Add(model);
+                return View("Feedback", model);
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/candidate.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Proje2.Models
+{
+    public class Candidate
+    {
+        [Required(ErrorMessage ="E-Mail is required.")]
+        public String? Email{get; set;}= String.Empty;
+
+         [Required(ErrorMessage ="Firstname is required.")]
+        public String? FirstName{get; set;}= String.Empty;
+        
+         [Required(ErrorMessage ="Lastname is required.")]
+        public String? LastName{get; set;}= String.Empty;
+        
+        public String? FullName => $"{FirstName} {LastName?.ToUpper()}";
+        public int? Age {get; set;}
+         public String? SelectedCourse {get; set;}= String.Empty;
+         public DateTime ApplyAt{get;set;}
+
+         public Candidate()
+        {
+           ApplyAt = DateTime.Now;
+        }
+
+    }
+}
